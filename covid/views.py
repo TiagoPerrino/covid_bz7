2fc2d2e5caa82baacba2476c3d90b42dab82b832
TiagoPerrino <82871798+TiagoPerrino@users.noreply.github.com>
2021-05-24 12:24:36 +0200

    Creación del primer HTML, con GET y POST

--- covid/views.py
+++ covid/views.py
@@ -1,3 +1,4 @@
+from flask import render_template, request
 from covid import app
 import csv
 import json
@@ -61,3 +62,11 @@ def casos(year, mes, dia=None):
 
     fichero.close()
     return json.dumps(res)
+
+
+@app.route('/incidenciasdiarias', methods = ['GET', 'POST'])
+def incidencia():
+    if request.method == 'GET':
+        return render_template("alta.html")
+    
+    return 'Se ha hecho un post'
